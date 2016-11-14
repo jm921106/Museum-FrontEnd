df53da9bcdfe4aea0c52c44fbfe7933c4ce74b6b
jm921106 <jm921106@naver.com>
2016-11-15 02:26:08 +0900

    pull please

--- www/js/temp_domain.js
+++ www/js/temp_domain.js
@@ -1,6 +1,6 @@
-/**
- * Created by superMoon on 2016-10-03.
- */
-
+// /**
+//  * Created by superMoon on 2016-10-03.
+//  */
+//
 // var temp_domain = 'http://54.214.221.63/';
 // var temp_domain = 'http://localhost:7777/';
